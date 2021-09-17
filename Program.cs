271a8326ca8827c340bffe77bc3164d9b14e39cf
Andreas Olsson <olsson.andreas@gmail.com>
2021-09-17 17:40:07 +0200

    Can convert video

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 
 namespace dotnet_ffmpeg_console
 {
   class Program
   {
     private static string ffmpegPath = "./lib/ffmpeg";
+    private static string exampleFilePath = "./example-file/example.mkv";
+    private static string outputDir = "./output/";
+      
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
       Console.WriteLine(ffmpegPath);
-      var list = new List<string>();
-      var psi = new ProcessStartInfo(ffmpegPath);
+      File.Delete(outputDir + "output.mp4");
+      var argStr = $"-i {exampleFilePath} -vcodec libx265 -crf 28 -r 30 {outputDir}output.mp4";
+
+      var psi = new ProcessStartInfo(ffmpegPath)
+      {
+        Arguments = argStr
+      };
       Process.Start(psi);
     }
   }
