16592757785419217beee25cc54d4a652dbd452e
Andreas Olsson <olsson.andreas@gmail.com>
2021-09-17 17:22:53 +0200

    added ffmpeg lib and it is possible to be ran through the Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace dotnet_ffmpeg_console
 {
-    class Program
+  class Program
+  {
+    private static string ffmpegPath = "./lib/ffmpeg";
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      Console.WriteLine(ffmpegPath);
+      var list = new List<string>();
+      var psi = new ProcessStartInfo(ffmpegPath);
+      Process.Start(psi);
     }
+  }
 }
