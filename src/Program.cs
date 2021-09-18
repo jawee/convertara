42d1a06f5bebf05ecfd173feae54c348abfaa09f
Andreas Olsson <olsson.andreas@gmail.com>
2021-09-18 08:04:59 +0200

    Restructured project and extracted videoconversion to its own class

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace dotnet_ffmpeg_console
+{
+  public class Program
+  {
+    private static string exampleFilePath = "./../example/example.mkv";
+    private static string outputPath = "./../output/output.mp4";
+      
+    static void Main(string[] args)
+    {
+      var videoConverter = new VideoConverter(exampleFilePath, outputPath);
+      videoConverter.ConvertVideo();
+    }
+  }
+}
---
+++ src/VideoConverter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace dotnet_ffmpeg_console
+{
+  public class VideoConverter
+  {
+    private static string ffmpegPath = "./lib/ffmpeg";
+    private string _inputPath;
+    private string _outputPath;
+    public VideoConverter(string inputPath, string outputPath) 
+    {
+      _inputPath = inputPath;
+      _outputPath = outputPath;
+    }
+
+    public bool ConvertVideo()
+    {
+      File.Delete(_outputPath);
+      var argStr = $"-i {_inputPath} -vcodec libx265 -crf 28 -r 30 {_outputPath}";
+
+      var psi = new ProcessStartInfo(ffmpegPath)
+      {
+        Arguments = argStr
+      };
+      using (var process = new Process())
+      {
+        var stopwatch = Stopwatch.StartNew();
+        process.StartInfo = psi;
+        process.Start();
+        process.WaitForExit();
+        stopwatch.Stop();
+
+        var res = false;
+        if(process.ExitCode == 0)
+        {
+          var elapsedTime = stopwatch.Elapsed;
+          res = true;
+          Console.WriteLine($"Video conversion complete {elapsedTime.ToString(@"hh\:mm\:ss\.fff")}");
+        } 
+        else 
+        {
+          Console.WriteLine("Error");
+        }
+        return res;
+      }
+    }
+  }
+}
+
