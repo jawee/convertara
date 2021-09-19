5d62798ecb1ef3ad5507d1a649db20f42a7c4f0b
Andreas Olsson <olsson.andreas@gmail.com>
2021-09-19 15:46:33 +0200

    feat: test project No tests yet.

---
+++ src/Convertara.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Convertara.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
