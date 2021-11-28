f137dcfb5ad270888fb05e055db1bcea721a5fdd
Andreas Olsson <olsson.andreas@gmail.com>
2021-11-28 13:21:54 +0100

    feat(async) Services are now async

--- src/Convertara.Infrastructure/Clients/TwitchClient.cs
+++ src/Convertara.Infrastructure/Clients/TwitchClient.cs
@@ -15,28 +15,28 @@ namespace Convertara.Infrastructure.Clients
         public TwitchClient()
         {
         }
-        public GetVideosResponse GetVideosForUserId(string userId, string token)
+        public async Task<GetVideosResponse> GetVideosForUserId(string userId, string token)
         {
             var url = $"https://api.twitch.tv/helix/videos?user_id={userId}";
-            var respParsed = MakeGetRequest<GetVideosResponse>(url, token);
+            var respParsed = await MakeGetRequest<GetVideosResponse>(url, token);
             return respParsed;
         }
 
-        public GetUsersResponse GetUserIdFromUsername(string username, string token)
+        public async Task<GetUsersResponse> GetUserIdFromUsername(string username, string token)
         {
             var url = $"https://api.twitch.tv/helix/users?login={username}";
-            var respParsed = MakeGetRequest<GetUsersResponse>(url, token);
+            var respParsed = await MakeGetRequest<GetUsersResponse>(url, token);
             return respParsed;
         }
 
-        public GetAccessTokenResponse GetToken(string clientId, string clientSecret)
+        public async Task<GetAccessTokenResponse> GetToken(string clientId, string clientSecret)
         {
             _clientId = clientId; 
             var authUrl = $"https://id.twitch.tv/oauth2/token?client_id={clientId}&client_secret={clientSecret}&grant_type=client_credentials";
-            var respParsed = MakePostRequest<GetAccessTokenResponse>(authUrl, "", null, false);
+            var respParsed = await MakePostRequest<GetAccessTokenResponse>(authUrl, "", null, false);
             return respParsed;
         }
-        private T MakePostRequest<T>(string url, string data, string token = null, bool authenticated = true) where T : ITwitchResponse
+        private async Task<T> MakePostRequest<T>(string url, string data, string token = null, bool authenticated = true) where T : ITwitchResponse
         {
             using(var httpClient = new HttpClient())
             {
@@ -48,7 +48,7 @@ namespace Convertara.Infrastructure.Clients
                     httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
                     httpClient.DefaultRequestHeaders.Add("Client-Id", _clientId);
                 }
-                var resp = httpClient.PostAsync(url, new StringContent(data, Encoding.UTF8, "application/json")).Result;
+                var resp = await httpClient.PostAsync(url, new StringContent(data, Encoding.UTF8, "application/json"));
                 var respContent = resp.Content.ReadAsStringAsync().Result;
                 var respParsed = JsonConvert.DeserializeObject<T>(respContent);
                 return respParsed;
@@ -56,7 +56,7 @@ namespace Convertara.Infrastructure.Clients
 
         }
 
-        private T MakeGetRequest<T>(string url, string token = null, bool authenticated = true)  where T : ITwitchResponse
+        private async Task<T> MakeGetRequest<T>(string url, string token = null, bool authenticated = true)  where T : ITwitchResponse
         {
             using(var httpClient = new HttpClient())
             {
@@ -68,7 +68,7 @@ namespace Convertara.Infrastructure.Clients
                     httpClient.DefaultRequestHeaders.Add("Client-Id", _clientId);
                 }
 
-                var resp = httpClient.GetAsync(url).Result;
+                var resp = await httpClient.GetAsync(url);
                 var respParsed = JsonConvert.DeserializeObject<T>(resp.Content.ReadAsStringAsync().Result);
                 return respParsed;
             }
